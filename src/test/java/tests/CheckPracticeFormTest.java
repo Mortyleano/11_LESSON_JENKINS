3a258b3d897720c81627a477b4ae8f7797052c47
Aleksandr Aleksandrov <mortyleano@MacBook-Pro-Aleksandr.local>
2024-07-29 21:58:31 +0300

    added fixes

--- src/test/java/tests/CheckPracticeFormTest.java
+++ src/test/java/tests/CheckPracticeFormTest.java
@@ -7,6 +7,7 @@ import pages.RegistrationPage;
 import pages.components.ModalWidget;
 import tests.utils.RandomUtils;
 
+import static io.qameta.allure.Allure.step;
 import static pages.components.ModalWidget.*;
 
 public class CheckPracticeFormTest extends TestBase {
@@ -32,18 +33,20 @@ public class CheckPracticeFormTest extends TestBase {
                 .setCity(randomUtils.city)
                 .submitRegistrationForm();
 
-        new ModalWidget().checkResultRegistrationForm(MODAL_STUDENT_NAME, randomUtils.firstName + " " + randomUtils.lastName)
-                .checkResultRegistrationForm(MODAL_STUDENT_EMAIL, randomUtils.userEmail)
-                .checkResultRegistrationForm(MODAL_STUDENT_GENDER, randomUtils.userGender)
-                .checkResultRegistrationForm(MODAL_STUDENT_MOBILE, randomUtils.userNumber)
-                .checkResultRegistrationForm(MODAL_STUDENT_DATE_OF_BIRTH, randomUtils.dayOfBirth + " "
-                        + randomUtils.monthOfBirth + ","
-                        + randomUtils.yearOfBirth
-                )
-                .checkResultRegistrationForm(MODAL_STUDENT_SUBJECT, randomUtils.userSubject)
-                .checkResultRegistrationForm(MODAL_STUDENT_HOBBIES, randomUtils.userHobby)
-                .checkResultRegistrationForm(MODAL_STUDENT_PICTURE, randomUtils.userFile)
-                .checkResultRegistrationForm(MODAL_STUDENT_ADDRESS, randomUtils.userCurrentAddress)
-                .checkResultRegistrationForm(MODAL_STUDENT_STATE_AND_CITY, randomUtils.state + " " + randomUtils.city);
+        step("Проверяем результаты регистрации в модальном окне",
+                () -> new ModalWidget().checkResultRegistrationForm(MODAL_STUDENT_NAME, randomUtils.firstName + " " + randomUtils.lastName)
+                        .checkResultRegistrationForm(MODAL_STUDENT_EMAIL, randomUtils.userEmail)
+                        .checkResultRegistrationForm(MODAL_STUDENT_GENDER, randomUtils.userGender)
+                        .checkResultRegistrationForm(MODAL_STUDENT_MOBILE, randomUtils.userNumber)
+                        .checkResultRegistrationForm(MODAL_STUDENT_DATE_OF_BIRTH, randomUtils.dayOfBirth + " "
+                                + randomUtils.monthOfBirth + ","
+                                + randomUtils.yearOfBirth
+                        )
+                        .checkResultRegistrationForm(MODAL_STUDENT_SUBJECT, randomUtils.userSubject)
+                        .checkResultRegistrationForm(MODAL_STUDENT_HOBBIES, randomUtils.userHobby)
+                        .checkResultRegistrationForm(MODAL_STUDENT_PICTURE, randomUtils.userFile)
+                        .checkResultRegistrationForm(MODAL_STUDENT_ADDRESS, randomUtils.userCurrentAddress)
+                        .checkResultRegistrationForm(MODAL_STUDENT_STATE_AND_CITY, randomUtils.state + " " + randomUtils.city
+                ));
     }
 }
